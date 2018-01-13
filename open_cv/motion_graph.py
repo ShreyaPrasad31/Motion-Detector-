9d4e0b78e3c92c7db93c7f49188fcfd75ac4578c
ShreyaPrasad31 <s9prasad@edu.uwaterloo.ca>
2018-01-13 15:37:26 -0500

    hover_tool featur +

--- open_cv/motion_graph.py
+++ open_cv/motion_graph.py
@@ -1,12 +1,19 @@
 from video_Capturing import df 
 
 from bokeh.plotting import figure,show,output_file
+from bokeh.models import HoverTool , ColumnDataSource
+
+df["Start_string"] = df["Start"].dt.str.strftime("%Y-%m-%d %H:%M:%S")
+df["End_string"] = df["End"].dt.strftime("%Y-%m-%d %H:%M:%S")
+cds = ColumnDataSource(df)
 
 
 p = figure(x_axis_type = 'datetime', height = 100, width = 500, reponsive = True, title = "Motion Graph")
-p.yaxis.minor_tick_color = None
+p.yaxis.minor_tick_line_color = None
 p.ygrid[0].ticker.desired_num_ticks = 1
 
-q = p.quad(left = df["Start"], right = df["End"], bottom=0, top= 1, color= "green")
+hover = HoverTool(tooltipe = [("Start_string ","@"), ("End_string", "@End")])
+p.add_tools(hover)
+q = p.quad(left = "Start", right = "End", bottom=0, top= 1, color= "green", souce = cds)
 output_file("Motion_graph.html")
 show(p)
